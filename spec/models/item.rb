28ae3210be96c89aaa452bea81877b1699ac21f1
Josi <junipertmcclellan@gmail.com>
2017-10-30 16:54:08 -0600

    spec item model

---
+++ spec/models/item.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+describe Item do
+
+  describe 'validations' do
+
+    it 'succeed with a name, description, price, image url, and category' do
+      item = Item.new(
+        name: 'item name',
+        description: 'item description',
+        price: 9.99,
+        image_url: 'example.com/item.jpg',
+        category: create(:category)
+      )
+      expect(item).to be_valid
+    end
+
+    it 'fail without a name' do
+      item = Item.new(
+        description: 'item description',
+        price: 9.99,
+        image_url: 'example.com/item.jpg',
+        category: create(:category)
+      )
+      expect(item).to_not be_valid
+    end
+
+    it 'fail without a description' do
+      item = Item.new(
+        name: 'item name',
+        price: 9.99,
+        image_url: 'example.com/item.jpg',
+        category: create(:category)
+      )
+      expect(item).to_not be_valid
+    end
+
+    it 'fail without a price' do
+      item = Item.new(
+        name: 'item name',
+        description: 'item description',
+        image_url: 'example.com/item.jpg',
+        category: create(:category)
+      )
+      expect(item).to_not be_valid
+    end
+
+    it 'fail without an image url' do
+      item = Item.new(
+        name: 'item name',
+        description: 'item description',
+        price: 9.99,
+        category: create(:category)
+      )
+      expect(item).to_not be_valid
+    end
+
+    it 'fail without a category' do
+      item = Item.new(
+        name: 'item name',
+        description: 'item description',
+        price: 9.99,
+        image_url: 'example.com/item.jpg'
+      )
+      expect(item).to_not be_valid
+    end
+  end
+
+  describe 'is associated with' do
+
+    before { @item = create(:item) }
+
+    it 'one category' do
+      expect(@item.category).to be_a(Category)
+    end
+
+    it 'many order items' do
+      @item.order_items << create(:order_item)
+      expect(@item.order_items.first).to be_a(OrderItem)
+    end
+
+  end
+
+end
