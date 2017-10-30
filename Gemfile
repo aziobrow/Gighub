ae0f4d97861b8de992783b84e98d8f408f434420
Aurora Ziobrowski <aziobrow@gmail.com>
2017-10-30 16:58:33 -0600

    added rspec setup and wrote first feature test

--- Gemfile
+++ Gemfile
@@ -34,6 +34,12 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'launchy'
+  gem 'capybara'
+  gem 'rspec-rails'
+  gem 'pry'
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
 end
 
 group :development do
---
+++ spec/features/visitor_can_see_list_of_all_items_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "when a visitor goes to the items index page" do
+  it "they can see a list of all items" do
+    item1 = create(:item)
+    item2 = create(:item)
+    visit items_path
+
+    expect(current_path).to eq('/items')
+    expect(page).to have_content("#{item1.title}")
+    expect(page).to have_content(item1.image)
+    expect(page).to have_content("#{item2.title}")
+    expect(page).to have_content(item2.image)
+  end
+end
