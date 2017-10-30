8f11aabd56fba4bfbc40691fa067a826b1ea4e14
Josi McClellan <junipertmcclellan@gmail.com>
2017-10-30 17:52:47 -0600

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     sequence(:title)  {|n| "Product #{n}"}
     sequence(:description)  {|n| "Description #{n}"}
     sequence(:price)  {|n| "Price: $#{n}"}
-    image "http://placebear.com/50/100"
+    image_url { "http://placebear.com/#{rand(175..225)}/#{rand(275..325)}" }
   end
 end
