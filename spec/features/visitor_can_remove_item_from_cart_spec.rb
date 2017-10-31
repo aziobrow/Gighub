498ca57c29d98f9f5428aaa86f07d6eb810b3224
Josi <junipertmcclellan@gmail.com>
2017-10-31 13:55:08 -0600

    spec deleting item from cart

---
+++ spec/features/visitor_can_remove_item_from_cart_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'When a visitor with items in their cart views their cart and clicks "Remove"' do
+
+  background do
+    deleted = create(:item, title: "DELETED_ITEM")
+    Cart.add_item(deleted.id)
+    visit carts_path
+    click_on "Remove"
+  end
+
+  it 'they stay on the page' do
+    expect(page).to have_current_path('/cart')
+  end
+
+  it 'they see a success message' do
+    expect(find('.success')).to have_content('Successfully removed DELETED_ITEM from your cart.')
+  end
+
+  it 'the title in the success message should be a link to that item' do
+    expect(find('.success')).to have_link('DELETED_ITEM', href: item_path(Item.first))
+  end
+
+  it 'they no longer see the item listed' do
+    expect(find('#cart-items')).to_not have_content('DELETED_ITEM')
+  end
+
+end
