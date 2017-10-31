339ba1abbe64182403ebd436f3291806237e2d43
Aurora Ziobrowski <aziobrow@gmail.com>
2017-10-31 13:27:49 -0600

    halfway through adding a cart, just created model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,10 @@
 class CartsController < ApplicationController
+
+  def create
+    item = Item.find(params[:item_id])
+    @cart.add_item(item.id)
+    session[:cart] = @cart.contents
+    flash[:success] = "You have successfully added #{item.title} to your cart!"
+    redirect_to items_path
+  end
 end
---
+++ app/models/cart.rb
@@ -0,0 +1,20 @@
+class Cart
+  attr_reader :contents
+
+  def initialize(initial_contents)
+    @contents = initial_contents || {}
+  end
+
+  def total_count
+    contents.values.sum
+  end
+
+  def add_item(id)
+    contents[id.to_s] = (contents[id.to_s] || 0) + 1
+  end
+
+  def count_of(id)
+    contents[id.to_s] || 0
+  end
+
+end
