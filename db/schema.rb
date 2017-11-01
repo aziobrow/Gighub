fad69ac1b4b7cc56479fe57eb611dc4328909010
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-01 12:41:17 -0600

    add slug migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031213410) do
+ActiveRecord::Schema.define(version: 20171101184027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "slug"
   end
 
   create_table "items", force: :cascade do |t|
