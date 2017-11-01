753b86183aac152ae2464ecbba8668de632f2f00
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-01 12:55:36 -0600

    change category_name to slug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find_by(slug: params[:category_name])
+    @category = Category.find_by(slug: params[:slug])
   end
 end
