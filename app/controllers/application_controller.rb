f3daa4d81814d34b4fb3d2a96858462111a33190
Josi <junipertmcclellan@gmail.com>
2017-11-01 13:48:38 -0600

    add #require_login helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include CurrentUserHelper
 
+  protect_from_forgery with: :exception
   before_action :set_cart
 
   def set_cart
     @cart ||= Cart.new(session[:cart])
   end
+
+  def require_login
+    redirect_to login_path unless logged_in?
+  end
+
 end
