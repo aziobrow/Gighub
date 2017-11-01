ab0f7ef0544cb94827805f62e526f12d6fa93278
Josi <junipertmcclellan@gmail.com>
2017-11-01 13:49:07 -0600

    add sessions#delete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,22 @@ class SessionsController < ApplicationController
     user = User.find_by(username: username)
 
     if user.nil?
-      flash[:error] = "Couldn't find user #{username}."
+      flash[:error] = "Couldn't find user #{username}"
       redirect_to login_path
     elsif user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "Logged in as #{username}"
       redirect_to dashboard_path
     else
-      flash[:error] = "Incorrect password for #{username}."
+      flash[:error] = "Incorrect password for #{username}"
       redirect_to login_path
     end
+  end
+
+  def destroy
+    reset_session
+    flash[:success] = "Successfully logged out"
+    redirect_back(fallback_location: root_path)
   end
 
 end
