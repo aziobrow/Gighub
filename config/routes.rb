df6c4a618869fad5cdbb25b8ef4b6b98c693f6b4
Josi <junipertmcclellan@gmail.com>
2017-11-01 13:47:28 -0600

    change logout route from post to delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   controller :sessions do
     get '/login' => :new
     post '/login' => :create
-    post '/logout' => :destroy
+    delete '/logout' => :destroy
   end
 
   resources :users, only: [:new, :create]
