689982e162e992e2f1f41cbb7fb5a59481fe4212
Josi <junipertmcclellan@gmail.com>
2017-11-01 22:25:05 -0600

    refactor cart spec
    spec removing last of item
    use item factory
    use #method_name

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,83 +1,74 @@
 require 'rails_helper'
 
 describe Cart do
-  subject { Cart.new({"1" => 2, "2" => 3, "3" => 4}) }
 
   before do
-    @item1 = Item.create!(
-              title: 'item title',
-              description: 'item description',
-              price: 10,
-              image_url: 'example.com/item.jpg',
-              category: create(:category),
-              id: 1)
-
-     @item2 = Item.create!(
-              title: 'item title',
-              description: 'item description',
-              price: 10,
-              image_url: 'example.com/item.jpg',
-              category: create(:category),
-              id: 2)
-
-    @item3 = Item.create!(
-             title: 'item title',
-             description: 'item description',
-             price: 10,
-             image_url: 'example.com/item.jpg',
-             category: create(:category),
-             id: 3)
+    item1 = create(:item, id: 1, price: 10)
+    item2 = create(:item, id: 2, price: 10)
+    item3 = create(:item, id: 3, price: 10)
+    @cart = Cart.new("1" => 2, "2" => 3, "3" => 4)
   end
 
-  describe '.contents' do
+  describe '#contents' do
     it 'stores contents in a hash' do
-      expect(subject.contents).to eq({"1" => 2, "2" => 3, "3" => 4})
+      expect(@cart.contents).to eq({"1" => 2, "2" => 3, "3" => 4})
     end
   end
 
-  describe '.total_count' do
+  describe '#total_count' do
     it 'can find the total count of cart contents' do
-      expect(subject.total_count).to eq(9)
+      expect(@cart.total_count).to eq(9)
     end
   end
 
-  describe '.add_item' do
+  describe '#add_item' do
     it 'can add a new item to contents' do
-      subject.add_item("4")
+      @cart.add_item("4")
 
-      expect(subject.contents).to eq({"1" => 2, "2" => 3, "3" => 4, "4" => 1})
+      expect(@cart.contents).to eq({"1" => 2, "2" => 3, "3" => 4, "4" => 1})
     end
 
     it 'can add another of the same item to contents' do
-      subject.add_item("1")
+      @cart.add_item("1")
 
-      expect(subject.contents).to eq({"1" => 3, "2" => 3, "3" => 4})
+      expect(@cart.contents).to eq({"1" => 3, "2" => 3, "3" => 4})
     end
   end
 
-  describe '.remove_item' do
+  describe '#remove_item' do
     it 'can remove an item from contents' do
-      subject.remove_item("2")
+      @cart.remove_item("2")
 
-      expect(subject.contents).to eq({"1" => 2, "2" => 2, "3" => 4})
+      expect(@cart.contents).to eq({"1" => 2, "2" => 2, "3" => 4})
+    end
+
+    it 'deletes item when quantity reaches zero' do
+      @cart.remove_item("1")
+      @cart.remove_item("1")
+
+      expect(@cart.contents).to eq({"2" => 3, "3" => 4})
     end
   end
 
-  describe '.count_of' do
+  describe '#count_of' do
     it 'can count the number of a single item in the cart' do
-      expect(subject.count_of("3")).to eq(4)
+      expect(@cart.count_of("3")).to eq(4)
+    end
+
+    it 'returns zero for nonexistant item' do
+      expect(@cart.count_of("0")).to eq(0)
     end
   end
 
-  describe '.cart_items' do
-    it 'can format cart items as strings' do
-      expect(subject.cart_items).to eq({@item1 => 2, @item2 => 3, @item3 => 4})
+  describe '#cart_items' do
+    it 'returns actual item records keyed to quantities' do
+      expect(@cart.cart_items).to eq({Item.first => 2, Item.second => 3, Item.third => 4})
     end
   end
 
-  describe '.total_price' do
-    it 'can find the total price of contents' do
-      expect(subject.total_price).to eq(90.00)
+  describe '#total_price' do
+    it 'can find the total price of all contents as an integer' do
+      expect(@cart.total_price).to be(90)
     end
   end
 end
