86a8ca812eb02065f816749cc127f2cf177a4505
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-02 11:54:34 -0600

    add admin_dashboard spec

---
+++ spec/features/users/admin_can_view_admin_dashboard_spec.rb
@@ -0,0 +1,26 @@
+# As a logged in Admin When I visit “/admin/dashboard” I will see a heading on the page that says “Admin Dashboard”
+#
+# As a registered user When I visit “/admin/dashboard” I get a 404
+#
+# As an unregistered user When I visit “/admin/dashboard” I get a 404
+require 'rails_helper'
+
+feature 'When I visit /admin/dashboard' do
+
+  scenario 'as an admin, they are taken to the admin dashboard' do
+    admin = create(:user, username: 'JaneAdmin89', password: 'pw', email: 'janeadmin89@example.com', status: 'admin')
+    allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(admin)
+    visit admin_dashboard_path
+  end
+
+  scenario 'as a logged in user, they get a 404' do
+    user = create(:user, username: 'JaneDoe89', password: 'pw', email: 'janedoe89@example.com')
+    allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(user)
+    visit admin_dashboard_path
+  end
+
+  scenario 'as a visitor, they get a 404' do
+    visit admin_dashboard_path
+  end
+
+end
