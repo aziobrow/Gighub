8bd0e8b05bc63d66f55b859686c4336f363d4ee3
Aurora Ziobrowski <aziobrow@gmail.com>
2017-11-02 15:20:04 -0600

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,3 @@ Category.create!([{name: "Technology"}, {name: "Home Repair"}])
 User.create!([{username: "aziobrow", password: "1234", email: "aziobrow@gmail.com", role: 1}, {username: "anna", password: "1234", email: "anna@gmail.com", role: 0}])
 Item.create!([{title: "Build a Website", image_url: "http://www.makeawebsiteguru.com/wp-content/uploads/2015/04/ways-to-build-a-website.png", price: 10, description: "I'll make you a cool website!", category: Category.first},
              {title: "Plumbing", image_url: "https://www.homedepot.com/hdus/en_US/DTCCOMNEW/fetch/Category_Pages/Kitchen/Sinks/bar-prep-sink-400x400-2.png", price: 50, description: "I'll unclog your sink!", category: Category.last}])
-Order.create!([{service_address: "123 Main St.", purchaser_name: "Aurora", status: 0, user: User.first}])
-OrderItem.create![{order: Order.first, item: Item.first, unit_cost: Item.first.price, quantity: 3}]
