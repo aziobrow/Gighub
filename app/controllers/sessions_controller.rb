6faba64f864cba10d44085e03d46bd7ae8f50e0d
Josi <junipertmcclellan@gmail.com>
2017-11-02 16:38:41 -0600

    update flash[:error] to flash[:danger]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
     user = User.find_by(username: username)
 
     if user.nil?
-      flash[:error] = "Couldn't find user #{username}"
+      flash[:danger] = "Couldn't find user #{username}"
       redirect_to login_path
     elsif user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "Logged in as #{username}"
       redirect_to dashboard_path
     else
-      flash[:error] = "Incorrect password for #{username}"
+      flash[:danger] = "Incorrect password for #{username}"
       redirect_to login_path
     end
   end
