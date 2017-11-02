d3cafb92a97ac61573bb94e1760cd6e374f8d2f4
Aurora Ziobrowski <aziobrow@gmail.com>
2017-11-02 11:51:45 -0600

    added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([{name: "Technology"}, {name: "Home Repair"}])
+User.create([{username: "aziobrow", password: "1234", email: "aziobrow@gmail.com"}])
+Item.create([{title: "Build a Website", image_url: "http://www.makeawebsiteguru.com/wp-content/uploads/2015/04/ways-to-build-a-website.png", price: 10, description: "I'll make you a cool website!", category: Category.first},
+             {title: "Plumbing", image_url: "https://www.homedepot.com/hdus/en_US/DTCCOMNEW/fetch/Category_Pages/Kitchen/Sinks/bar-prep-sink-400x400-2.png", price: 50, description: "I'll unclog your sink!", category: Category.last}])
+Order.create([{shipping_address: "123 Main St.", user: User.first}])
