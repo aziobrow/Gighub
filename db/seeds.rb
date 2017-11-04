08d98d0339ce0c4cb91f674c7042ed20eacaa8a3
Josi <junipertmcclellan@gmail.com>
2017-11-04 15:09:28 -0600

    have seeds use database cleaner

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Item.destroy_all
-Order.destroy_all
-OrderItem.destroy_all
-Category.destroy_all
-User.destroy_all
-
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
 
 Category.create!([{
   name: "Technology"
