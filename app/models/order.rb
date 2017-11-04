f29daaedb22d762628404afda94e4ce1ee05cfca
Josi <junipertmcclellan@gmail.com>
2017-11-04 14:47:19 -0600

    use cents and default parent fields

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,11 +4,18 @@ class Order < ApplicationRecord
   has_many :order_items
 
   validates_presence_of :purchaser_name, :status
+  before_validation :save_purchaser_name
 
   enum status: ['ordered', 'paid', 'cancelled', 'completed']
 
   def total_cost
-    order_items.sum('unit_cost * quantity') / 100.0
+    order_items.sum('unit_cost * quantity')
+  end
+
+private
+
+  def save_purchaser_name
+    self.purchaser_name ||= user && user.username
   end
 
 end
