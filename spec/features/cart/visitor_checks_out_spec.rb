05253d60307e85cd36ae34eb3126a083dee3dcce
Aurora Ziobrowski <aziobrow@gmail.com>
2017-11-04 12:09:58 -0600

    started guest checkout spec

---
+++ spec/features/cart/visitor_checks_out_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "a visitor visits their cart" do
+  it "they do not see an option to checkout" do
+    
+
+  end
+
+  it "they see an option to checkout after signing up" do
+
+  end
+
+  it "they see all of the data that was there before logging in" do
+
+  end
+end
