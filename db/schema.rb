1d5d821db6495a141f7a32df68b043f3fc881d25
Josi <junipertmcclellan@gmail.com>
2017-11-04 17:15:38 -0600

    add active boolean to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102011521) do
+ActiveRecord::Schema.define(version: 20171104223159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171102011521) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "slug"
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "items", force: :cascade do |t|
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171102011521) do
     t.integer "price", null: false
     t.text "description", null: false
     t.bigint "category_id"
+    t.boolean "active", default: true, null: false
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
