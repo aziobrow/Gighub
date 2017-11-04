89b50d503978c021541b2a5fb0ef922a3923903b
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-04 13:23:23 -0600

    clean

--- spec/features/admin/users/admin_can_view_admin_dashboard_spec.rb
+++ spec/features/admin/users/admin_can_view_admin_dashboard_spec.rb
@@ -1,8 +1,3 @@
-# As a logged in Admin When I visit “/admin/dashboard” I will see a heading on the page that says “Admin Dashboard”
-#
-# As a registered user When I visit “/admin/dashboard” I get a 404
-#
-# As an unregistered user When I visit “/admin/dashboard” I get a 404
 require 'rails_helper'
 
 feature 'When I visit /admin/dashboard' do
