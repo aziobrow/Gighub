cf6f748bc87231987d03e2377ae3804392d1acfc
Aurora Ziobrowski <aziobrow@gmail.com>
2017-11-04 12:43:22 -0600

    option to checkout after logging in

--- spec/features/cart/visitor_checks_out_spec.rb
+++ spec/features/cart/visitor_checks_out_spec.rb
@@ -1,16 +1,35 @@
 require 'rails_helper'
 
 describe "a visitor visits their cart" do
-  it "they do not see an option to checkout" do
-    
-
+  before do
+    item = create(:item)
+    visit items_path
+    first("input.add-to-cart").click
+    find("#go-to-cart").click
   end
 
-  it "they see an option to checkout after signing up" do
+    it "they do not see an option to checkout" do
+      expect(current_path).to eq(cart_path)
+      expect(page).to have_content(Item.first.title)
+      expect(page).to have_content(Item.first.price)
+      expect(page).to have_content(Item.first.description)
+      expect(first("img")['alt']).to have_content(Item.first.title)
+      expect(page).to have_content("Login or Create Account to Checkout")
+    end
 
-  end
+    it "they see an option to checkout after signing up" do
+      click_on("Login or Create Account to Checkout")
 
-  it "they see all of the data that was there before logging in" do
+      expect(current_path).to eq(login_path)
 
-  end
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit cart_path
+
+      expect(page).to have_content("Checkout")
+    end
+
+    xit "they see all of the data that was there before logging in" do
+
+    end
 end
