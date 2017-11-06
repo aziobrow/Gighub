9fe692747244be05d1df719b59c81b25ffd4dcaf
Josi <junipertmcclellan@gmail.com>
2017-11-05 19:48:16 -0700

    debug admin item creation feature

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,29 +4,33 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @categories = Category.all
   end
 
   def create
     item = Item.new(item_params)
     if item.save
+      item.price = (item.price.to_f * 100).to_i
       flash[:success] = "#{item.title} successfully created"
       redirect_to item_path(item)
     else
       flash[:danger] = item.errors.full_messages.join("\n")
-      redirect_to new_admin_item_page
+      redirect_to new_admin_item_path
     end
   end
 
 private
 
   def item_params
-    params.require(:item).permit(
+    raw_params = params.require(:item).permit(
       :title,
       :description,
       :price,
       :category_id,
       :image_url
     )
+    raw_params[:price] = (raw_params[:price].to_f * 100).to_i
+    raw_params
   end
 
 end
--- spec/features/items/visitor_can_see_a_single_item_spec.rb
+++ spec/features/items/visitor_can_see_a_single_item_spec.rb
@@ -11,19 +11,19 @@ feature "when a visitor goes to the item show page"do
   end
 
   scenario "they see an image of the item" do
-    expect(first("img")['alt']).to have_content "#{item.title}"
+    expect(first("img")['alt']).to have_content(item.title)
   end
 
   scenario "they see the item title" do
-    expect(page).to have_content("#{item.title}")
+    expect(page).to have_content(item.title)
   end
 
   scenario "they see the price of the item" do
-    expect(page).to have_content("$#{item.price}")
+    expect(page).to have_content('$%.2f' % (item.price / 100.0))
   end
 
   scenario "they see the item's description" do
-    expect(page).to have_content("#{item.description}")
+    expect(page).to have_content(item.description)
   end
 
   scenario "they see a link to add the item to their cart" do
