b609d5ef1dc0f4bf1b200b7cf8b7070db52a8756
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-05 22:14:50 -0700

    make test log in

--- spec/features/cart/user_checks_out_spec.rb
+++ spec/features/cart/user_checks_out_spec.rb
@@ -1,31 +1,48 @@
 require 'rails_helper'
 
-feature 'a user visits their cart' do
-  scenario 'the user checks out' do
+feature 'the cart page' do
+  context 'when a user places and order' do
     before do
       create(:item)
       visit items_path
       first("input.add-to-cart").click
       find("#go-to-cart").click
-
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit cart_path
     end
 
-    it 'the order is placed' do
+    scenario 'the order is placed' do
+      user = create(:user)
+      allow_any_instance_of(CurrentUserHelper).to receive(:current_user).and_return(user)
+      visit cart_path
+      save_and_open_page
+      click_on("Checkout")
+
       expect(user.orders.last.status).to eq('ordered')
     end
 
-    it 'the current page is /orders' do
+    scenario 'the current page is /orders' do
+      user = create(:user)
+      allow_any_instance_of(CurrentUserHelper).to receive(:current_user).and_return(user)
+      visit cart_path
+      click_on("Checkout")
+
       expect(current_path).to eq(orders_path)
     end
 
-    it 'the user sees a success message' do
+    scenario 'the user sees a success message' do
+      user = create(:user)
+      allow_any_instance_of(CurrentUserHelper).to receive(:current_user).and_return(user)
+      visit cart_path
+      click_on("Checkout")
+
       expect(page).to have_content('Order was successfully placed')
     end
 
-    it 'the user sees the placed order in a table' do
+    scenario 'the user sees the placed order in a table' do
+      user = create(:user)
+      allow_any_instance_of(CurrentUserHelper).to receive(:current_user).and_return(user)
+      visit cart_path
+      click_on("Checkout")
+
       expect(page).to have_content(Item.first.title)
       expect(page).to have_content(Item.first.price)
       expect(page).to have_content(Item.first.description)
