15d7b219267f82685be751f89141a8f5ebe14d8e
Josi <junipertmcclellan@gmail.com>
2017-11-05 18:02:53 -0700

    add feature spec for admin item creation

---
+++ spec/features/admin/items/admin_can_create_an_item_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'When an admin creates an item' do
+
+  background do
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(create(:admin))
+    create(:category, name: 'Things')
+
+    visit new_admin_item_path
+    fill_in 'item[title]', with: 'something unique'
+    fill_in 'item[description]', with: 'something descriptive'
+    fill_in 'item[price]', with: '9.99'
+    select 'Things', from: 'item[category_id]'
+  end
+
+  scenario 'they are taken to the item page' do
+    expect(page).to have_current_path(item_path(Item.last))
+  end
+
+  scenario 'they see the details they just entered' do
+    expect(page).to have_content('something unique')
+               .and have_content('something descriptive')
+               .and have_content('$9.99')
+               .and have_content('Things')
+  end
+
+  context 'with the optional image url' do
+    scenario 'they see the image show up on the page' do
+      fill_in 'item[image_url]', with: 'http://backend.turing.io/assets/images/full-logo.png'
+      click_on 'Create Item'
+      expect(page).to have_css('img[src$="full-logo.png"]')
+    end
+  end
+
+end
