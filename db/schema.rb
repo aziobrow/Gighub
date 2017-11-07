5ba796347379268d1eaff5ebb3c2a7ffa3b93e95
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-07 14:30:25 -0700

    add name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107212319) do
+ActiveRecord::Schema.define(version: 20171107212858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171107212319) do
     t.string "password_digest", null: false
     t.string "email", null: false
     t.integer "role", default: 0, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
