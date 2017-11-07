92355774be074a9877f72da49a3b28e8f9b06e86
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-07 14:32:18 -0700

    add address to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107212858) do
+ActiveRecord::Schema.define(version: 20171107213108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171107212858) do
     t.string "email", null: false
     t.integer "role", default: 0, null: false
     t.string "name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
