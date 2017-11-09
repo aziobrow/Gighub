0713fc1e5806d7b10839b171b554146f5fc3a2a1
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-08 23:18:56 -0700

    Update admin_can_view_an_order_spec.rb

--- spec/features/admin/orders/admin_can_view_an_order_spec.rb
+++ spec/features/admin/orders/admin_can_view_an_order_spec.rb
@@ -15,7 +15,7 @@ feature 'When an admin visits an order page' do
   end
 
   scenario 'they can see the order\'s date and time' do
-    expect(page).to have_content(Order.first.created_at.strftime("%m/%d/%Y at %I:%M %p"))
+    expect(page).to have_content(Order.first.format_created_time)
   end
 
   scenario 'they see the order status' do
